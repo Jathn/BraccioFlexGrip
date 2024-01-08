8a71dae6e884b41d04c04baa2aedcf81f8c836b1
Jathn <jonathan.ramstedt@gmail.com>
2024-01-08 14:53:56 +0200

    Added src

---
+++ src/RobotController.hpp
@@ -0,0 +1,30 @@
+#ifndef ROBOTCONTROLLER_HPP
+#define ROBOTCONTROLLER_HPP
+
+/**
+ * @file RobotController.hpp
+ * @author Jonathan Ramstedt (jonathan.ramstedt@gmail.com)
+ * @brief Controller class for a the robot arm.
+ *  
+ * This class serves as the main controller for a Braccio Arduino robot arm. It handles the
+ * coordination and control of the robot arm's movements and functionalities.
+ * 
+ * @version 0.1
+ * @date 2024-01-08
+ * 
+ */
+
+class RobotController {
+public:
+    /**
+     * @brief Construct a new Robot Controller object with standard .
+     * 
+     * This constructor initializes the RobotController object.
+     */
+    RobotController();
+
+private:
+
+};
+
+#endif ROBOTCONTROLLER_HPP
